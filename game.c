c96ae7394102a18cab9fce4cde22ad75c8f42702
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-06-30 10:07:57 +0100

    Added pointers to the game for memory location

--- game.c
+++ game.c
@@ -2,15 +2,15 @@
 
 void go_south_east(int lat, int lon)
 {
-  lat = lat - 1;
-  lon = lon + 1;
+  *lat = *lat - 1;
+  *lon = *lon + 1;
 }
 
 int main()
 {
   int latitude = 32;
   int longitude = -64;
-  go_south_east(latitude, longitude);
+  go_south_east(&latitude, &longitude);
   printf("Avast! Now at: [%i, %i]\n", latitude, longitude);
   return 0;
 }
